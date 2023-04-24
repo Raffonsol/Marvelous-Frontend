68c1afc999e79e322975e0a79538246f3f160afd
Ricardo Affonso <>
2023-04-23 20:47:23 -0700

    remo extra slash

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,6 +1,6 @@
 const prod = {
   url: {
-   API_URL: 'https://p3hntfqq4d.execute-api.us-east-1.amazonaws.com/',
+   API_URL: 'https://p3hntfqq4d.execute-api.us-east-1.amazonaws.com',
   },
 };
  const dev = {
